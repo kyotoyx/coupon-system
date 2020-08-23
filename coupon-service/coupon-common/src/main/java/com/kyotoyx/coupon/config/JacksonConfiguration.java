ec1f1f4aae47f8294d516742b1a429756468aa8f
Yixiu <yc3691@nyu.edu>
2020-08-22 22:07:25 -0400

    Added common configuration using common package.

---
+++ coupon-service/coupon-common/src/main/java/com/kyotoyx/coupon/config/JacksonConfiguration.java
@@ -0,0 +1,20 @@
+package com.kyotoyx.coupon.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.text.SimpleDateFormat;
+
+@Configuration
+public class JacksonConfiguration {
+
+    @Bean
+    public ObjectMapper getObjectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setDateFormat(new SimpleDateFormat(
+                "yyyy-MM-dd HH-mm-ss"
+        ));
+        return mapper;
+    }
+}
