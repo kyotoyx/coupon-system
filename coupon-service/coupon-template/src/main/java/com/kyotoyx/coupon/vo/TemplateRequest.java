b0ea86ef56929a3eb0a8dfd652b17a12044a87ad
kyotoyx <chenyixiu26@gmail.com>
2020-10-25 15:50:23 -0400

    Added TemplateRequest value object.

---
+++ coupon-service/coupon-template/src/main/java/com/kyotoyx/coupon/vo/TemplateRequest.java
@@ -0,0 +1,48 @@
+package com.kyotoyx.coupon.vo;
+
+
+import com.kyotoyx.coupon.constant.CouponCategory;
+import com.kyotoyx.coupon.constant.DistributeTarget;
+import com.kyotoyx.coupon.constant.ProductLine;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TemplateRequest {
+
+    private String name;
+
+    private String logo;
+
+    private String desc;
+
+    private String category;
+
+    private Integer productLine;
+
+    private Integer count;
+
+    private Long userId;
+
+    private Integer target;
+
+    private TemplateRule rule;
+
+    public boolean validate() {
+        boolean stringValid = StringUtils.isNotEmpty(name) &&
+                StringUtils.isNotEmpty(logo) &&
+                StringUtils.isNotEmpty(desc);
+
+        boolean enumValid = null != CouponCategory.of(category)
+                && null != ProductLine.of(productLine)
+                && null != DistributeTarget.of(target);
+        boolean numValid = count > 0 && userId > 0;
+
+
+        return stringValid && enumValid && numValid && rule.validate();
+    }
+}
